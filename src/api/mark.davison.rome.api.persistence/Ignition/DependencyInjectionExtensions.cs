e05f693ae4f34f40f3a130d098b0acb4a4e627f3
mark.davison <markdavison0+github@gmail.com>
2026-01-06 17:43:00 +1300

    fix recursive

--- src/api/mark.davison.rome.api.persistence/Ignition/DependencyInjectionExtensions.cs
+++ src/api/mark.davison.rome.api.persistence/Ignition/DependencyInjectionExtensions.cs
@@ -9,7 +9,6 @@ public static class DependencyInjectionExtensions
         params Type[] migrationTypes)
     {
         return services
-            .AddPersistence(productionMode, databaseSettings)
             .AddDatabase<RomeDbContext>(productionMode, databaseSettings, migrationTypes)
             .AddCoreDbContext<RomeDbContext>()
             .AddTransient<IDataSeeder>(_ => new RomeDataSeeder(_.GetRequiredService<IDateService>(), productionMode));
