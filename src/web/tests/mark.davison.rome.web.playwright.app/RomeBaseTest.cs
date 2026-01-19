5f511a494704d204946ccbec0289571bc90a7a37
mark.davison <markdavison0+github@gmail.com>
2026-01-19 20:42:36 +1300

    Fix ui tests running in ci

--- src/web/tests/mark.davison.rome.web.playwright.app/RomeBaseTest.cs
+++ src/web/tests/mark.davison.rome.web.playwright.app/RomeBaseTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace mark.davison.rome.web.playwright.app;
 
+// TODO: Common interface with all properties that are shared between SkipBaseTest and LoggedInTest
 #if SKIP_TUNIT_TESTS
 public abstract class RomeBaseTest : SkipBaseTest
 #else
@@ -13,5 +14,17 @@ public abstract class RomeBaseTest : LoggedInTest
 
     protected string MakeAccountNumber() => _faker.Finance.Account();
     protected string MakeAccountName() => _faker.Finance.AccountName();
+
+#if SKIP_TUNIT_TESTS
+
+    [Before(Test)]
+    public void BeforeTest()
+    {
+        Skip.Test("Dont run ui tests from main solution");
+    }
+
+    protected DashboardPage Dashboard => throw new NotImplementedException();
+#else
     protected DashboardPage Dashboard => new(CurrentPage, AppSettings);
+#endif
 }
